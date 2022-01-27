0f07e1d1e9ebd986bf5afb9cd3d8a4c1b8bf811c
AngeloLista <abstractplayer@gmail.com>
2022-01-27 16:49:28 +0100

    binded img index

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ Vue.config.devtools = true;
 const app = new Vue ({
     el: '#root',
     data: {
+        currentIndex: 0,
         images: [
             'images/image1.jpg',
             'images/image2.jpg',
