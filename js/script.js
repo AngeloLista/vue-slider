7aa40e592aab6c483e06cd62d99af488e123a594
AngeloLista <abstractplayer@gmail.com>
2022-01-27 16:42:11 +0100

    binded img url to vuejs file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log('JS OK');
+
+Vue.config.devtools = true;
+
+const app = new Vue ({
+    el: '#root',
+    data: {
+        images: [
+            'images/image1.jpg',
+            'images/image2.jpg',
+            'images/image3.jpg',
+            'images/image4.jpg',
+        ]
+    },
+    methods: {
+
+    }
+})
